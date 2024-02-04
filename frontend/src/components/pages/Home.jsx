7221b0d531cbae5ff8e5e39c7093d6f3ac958fc5
Wasay Khan <wkhan19@depaul.edu>
2024-02-04 16:50:51 -0600

    Homepage WIP

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import './Home.css'; 
 
 export const Home = () => {
-  return <h1>Home</h1>;
+  return ( 
+  <div>
+    <h1 className = "Header">
+      Welcome to <span style={{ fontStyle: 'italic' }}> Cypher's</span> Cybersecurity Clinic
+    </h1>;
+
+    <div className = "missionStatement"> 
+    <p>
+      A cybersecurity clinic is similar in concept to a Law Clinic in that students perform needed professional services to under-resourced external clients. In exchange, students gain a transformative educational experiencefrom working on real-world projects in their area of study, while providing a public good.
+    </p>
+    <p>
+      Our clinic focuses on three main areas: 
+    </p>
+    <ul>
+      <li>General security risk assessment</li>
+      <li>Audit</li>
+      <li>Policy Review</li>
+    </ul>
+    
+    </div>
+
+
+
+
+  </div>
+  );
 };
