2c9bbf554739b18404a76ebc7f89b34a0ffb6f27
Ali Raufi <aliraufi@Sadiyahs-MacBook-Air.local>
2024-02-20 23:59:51 -0600

    extra files used by aboutUS, also ignore the name for this branch should have been aboutUs

---
+++ frontend/src/components/pages/about/FacultyData.jsx
@@ -0,0 +1,9 @@
+import { johnSmithImg, janeSmithImg, jimSmithImg } from '../../../images';
+
+const FacultyData = [
+  { id: 1, photo: johnSmithImg, name: 'John Doe', title: 'Project Manager', college: 'College Name', facultyPage: 'URL' },
+  { id: 2, photo: janeSmithImg, name: 'Jane Smith', title: 'Lead Developer', college: 'College Name', facultyPage: 'URL' },
+  { id: 3, photo: jimSmithImg, name: 'Jim Smith', title: 'Backend Developer', college: 'College Name', facultyPage: 'URL' },
+];
+
+export default FacultyData;
