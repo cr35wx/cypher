f4034ba96c286d516b4480d523b7740292f4adf0
Ali Raufi <aliraufi@Sadiyahs-MacBook-Air.local>
2024-02-25 17:00:23 -0600

    faculty list updated for depaul members w/ filtering added

--- frontend/src/components/pages/about/FacultyMember.jsx
+++ frontend/src/components/pages/about/FacultyMember.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function FacultyMember({ photo, name, title, college, facultyPage }) {
   return (
     <div className="flex flex-col items-center mb-8">
-      <img src={photo} alt={name} className="w-20 h-20 rounded-full mb-4 shadow-md hover:shadow-lg object-cover object-center" /> 
+      <img src={photo} alt={name} className="w-20 h-20 rounded-full mb-4 shadow-md hover:shadow-lg object-cover object-center" />
       <div className="text-center">
         <p className="text-lg font-semibold text-white mb-1">{name}</p>
         <p className="text-white mb-1">{title}</p>
