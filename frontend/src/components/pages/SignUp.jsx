12b923b9aa3e89b98cd104110075eeaef199ee8e
Ali Raufi <aliraufi@Sadiyahs-MacBook-Air.local>
2024-02-25 20:11:46 -0600

    email input field changes

--- frontend/src/components/pages/SignUp.jsx
+++ frontend/src/components/pages/SignUp.jsx
@@ -16,7 +16,6 @@ const SignUp = () => {
 
     const [user, setUser] = useState('');
     const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
 
     const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
