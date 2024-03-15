54d284aee9d5f71a418bf4d6f2047cf3dc15fde2
araufi <raufiali34@gmail.com>
2024-03-15 18:43:36 -0500

    useEffect not use in Navbar.jsx so i removed it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../images/logo.png";
